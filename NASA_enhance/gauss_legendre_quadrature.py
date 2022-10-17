beb1b3e0fde88711d81d21936acf9b3c78a3c281
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-10-17 20:47:50 -0300

    pernicious

--- NASA_enhance/gauss_legendre_quadrature.py
+++ NASA_enhance/gauss_legendre_quadrature.py
@@ -1,47 +1,39 @@
-from __future__ import print_function
-
 import numpy as np
+import time
 import sys
-import benchmark_decorator as dectimer
+import scipy as sp
+from intpy.intpy import initialize_intpy, deterministic
 
-#--------------------
-# Function: integrand
-#--------------------
-integrand = lambda x: np.exp(x)
 
-#-----------------------------
-# Function: compute_quadrature
-#-----------------------------
-if(len(sys.argv) < 3): 
-    environment = 'host'
-else: environment = sys.argv[2]
+def integrand(x): return np.exp(x)
 
-@dectimer.bench_time(3, sys.argv[1], environment)
+
+@deterministic
 def compute_quadrature(n):
-    """
-      Perform the Gauss-Legendre Quadrature at the prescribed order n
-    """
     a = -3.0
     b = 3.0
-
-    # Gauss-Legendre (default interval is [-1, 1])
     x, w = np.polynomial.legendre.leggauss(n)
 
-    # Translate x values from the interval [-1, 1] to [a, b]
     t = 0.5*(x + 1)*(b - a) + a
 
     gauss = sum(w * integrand(t)) * 0.5*(b - a)
+    return gauss
 
-if len(sys.argv) < 1:
-    print('Usage:')
-    print('     python ' + sys.argv[0] + ' N')
-    print('Please specify the order of the quadrature.')
-    sys.exit()
 
-order = int(sys.argv[1])
+@initialize_intpy(__file__)
+def main(A):
+    if len(sys.argv) < 1:
+        print('Usage:')
+        print('     python ' + sys.argv[0] + ' N')
+        print('Please specify the order of the quadrature.')
+        sys.exit()
 
-print('Gauss-Legendre Quadrature of order: ', order)
 
-compute_quadrature(order)
-
-print(' ')
+if __name__ == "__main__":
+    order = int(sys.argv[1])
+    print('Gauss-Legendre Quadrature of order: ', order)
+    compute_quadrature(order)
+    print(' ')
+    start = time.perf_counter()
+    main(order)
+    print(time.perf_counter()-start)
---
+++ NASA_enhance/pernicious.py
@@ -0,0 +1,46 @@
+import numpy as np
+import time
+import sys
+import scipy as sp
+from intpy.intpy import initialize_intpy, deterministic
+
+
+def is_prime_number(n):
+    if n in (2, 3):
+        return True
+    if 2 > n or 0 == n % 2:
+        return False
+    if 9 > n:
+        return True
+    if 0 == n % 3:
+        return False
+
+    return not any(map(
+        lambda x: 0 == n % x or 0 == n % (2 + x),
+        range(5, 1 + int(n ** 0.5), 6)
+    ))
+
+
+def get_number_of_ones(n): 
+    return bin(n).count("1")
+
+@deterministic
+def find_pernicious_numbers(n):
+    i = 1
+    counter = 0
+    while counter < n:
+        if is_prime_number(get_number_of_ones(i)):
+            counter += 1 
+        i += 1
+    return i-1, counter
+
+@initialize_intpy(__file__)
+def main(n):
+    return find_pernicious_numbers(n)
+
+if __name__ == "__main__":
+    N = int(sys.argv[1])
+    find_pernicious_numbers(N)
+    start = time.perf_counter()
+    main(N)
+    print(time.perf_counter()-start)
